ef9162982e5d52660e8a3d1bf6bfe7f4c14ce9d5
Cesar Narvaez <cesar.teacher24@gmail.com>
2025-01-25 19:31:46 -0500

    Implementa impresion

--- src/views/caja/components/crearMesa.vue
+++ src/views/caja/components/crearMesa.vue
@@ -272,7 +272,7 @@ const cerrarMesa = async (mesaContabilidadData) => {
   isLoading.value = false;
   cleanFilter();
   emit("obtenerMesasPorCaja");
-  // await imprimirFactura();
+  await imprimirFactura();
 };
 
 const crearMesaContabilidad = async (data) => {
